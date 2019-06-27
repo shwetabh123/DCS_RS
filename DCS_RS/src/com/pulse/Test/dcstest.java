727ecf6b54a20343c3614e81cb0abd594ade2b7a
shwetabh123 <shwetabh.srivastava@gmail.com>
2019-06-27 07:47:14 +0530

    master

---
+++ DCS_RS/src/com/pulse/Test/dcstest.java
@@ -0,0 +1,19 @@
+package com.pulse.Test;
+
+public class dcstest {
+	
+	
+	
+	
+	
+	
+	public static void main(String args[]) 
+    { 
+        double a = 12.123; 
+        double b = 12.456; 
+  
+        // prints the maximum of two numbers 
+        System.out.println(Math.max(a, b)); 
+    } 
+
+}
