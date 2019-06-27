e03ed8bdc2ed5c07e185a0711befe8c46a14104d
shwetabh123 <shwetabh.srivastava@gmail.com>
2019-06-27 07:51:39 +0530

    develop

--- DCS_RS/src/com/pulse/Test/dcstest.java
+++ DCS_RS/src/com/pulse/Test/dcstest.java
@@ -4,13 +4,10 @@ public class dcstest {
 	
 	
 	
-	
-	
-	
 	public static void main(String args[]) 
     { 
-        double a = 12.123; 
-        double b = 12.456; 
+        int a = 23; 
+        int b = -23; 
   
         // prints the maximum of two numbers 
         System.out.println(Math.max(a, b)); 
