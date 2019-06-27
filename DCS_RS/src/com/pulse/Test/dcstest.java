5d8d22ab1d6151550739c002a8ca3caf774d6882
shwetabh123 <shwetabh.srivastava@gmail.com>
2019-06-27 08:15:28 +0530

    m

--- DCS_RS/src/com/pulse/Test/dcstest.java
+++ DCS_RS/src/com/pulse/Test/dcstest.java
@@ -6,7 +6,24 @@ public class dcstest {
 	//shwetabh
 	public static void main(String args[]) 
     { 
-        int a = 23; 
+       
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		int a = 23; 
         int b = -23; 
   
         // prints the maximum of two numbers 
